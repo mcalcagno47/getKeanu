20e3faaa5539b3aa002f3fcd85ac5dd7dc742439
Mark Calcagno <mcalcagn@gmail.com>
2023-06-12 15:39:17 -0500

    made ranking sheet

--- src/components/pages/Rankings.js
+++ src/components/pages/Rankings.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 export default function Rankings() {
   return (
-    <div id="infoDiv" className='info-container'>
-      <h2>Rankings</h2>
-      <p>
-          Test Text for Rankings
-      </p>
-    </div>
+      <div className='rankListContainer'>
+        <div id='Amandas-rank' className='rankList'>
+            <h3>Amanda's List</h3>
+            <ol>
+                <li>John Wick</li>
+            </ol>
+        </div>
+        <div id='Sarahs-rank' className='rankList'>
+        <h3>Sarah's List</h3>
+            <ol>
+                <li>John Wick</li>
+            </ol>
+        </div>
+        <div id='Marks-rank' className='rankList'>
+        <h3>Mark's List</h3>
+            <ol>
+                <li>John Wick</li>
+            </ol>
+        </div>
+      </div>
   );
 }
